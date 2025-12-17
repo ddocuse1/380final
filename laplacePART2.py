304fa4d05b0be9f596e6a9d2974c53ead1b879c8
Dustin Docusen <dustindocusen@public-66-243.emich.edu>
2025-12-16 19:58:42 -0500

    submission

---
+++ laplacePART2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  4 11:36:59 2025
+
+@author: dustindocusen
+"""
+
+import numpy as np
+from matplotlib import pyplot as pp
+from matplotlib.animation import FuncAnimation as FA
+
+
+dx = dy = 0.1
+a,b = 10,10
+maxV = 1
+x = np.arange(0, dx + a, dx, dtype=float)
+y = np.arange(0, dy + b, dy, dtype=float)
+n = len(x)
+
+X,Y = np.meshgrid(x,y)
+
+V = np.zeros_like(X)
+
+fig = pp.figure()
+ax = pp.axes(xlim=[0,a],ylim=[0,b])
+
+def init():
+    ax.set_xlabel('x (cm)')
+    ax.set_ylabel('y (cm)')
+    return []
+
+def update(m):
+    global V
+    newV = np.zeros_like(V)
+    for i in range(V.shape[0]):
+        newV[0, :] = (np.cos(np.pi * X[0, :] / a))**2
+        newV[-1, :] = -(np.sin(np.pi * X[-1, :] / a))**2
+        
+    newV[:,-1] = 0
+    newV[:,0] = 0
+
+    for i in range(1, V.shape[0]-1):
+        for j in range(1, V.shape[1]-1):
+            newV[i,j] = (1/4)*(
+                V[i+1,j] + V[i-1,j] +
+                V[i,j+1] + V[i,j-1]
+                )
+            
+    V = newV
+    ax.cla()
+    cont = ax.contourf(X,Y,V,levels=100)
+    return cont.collections
+
+
+myanim = FA(fig,update,init_func=init,interval=10)
+
+pp.show()
